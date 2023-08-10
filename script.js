ffa5105068e2c3fc0a6bdf486d6bbf6c1a753205
Amo Ikiror <amoikiror@gmail.com>
2023-08-10 18:10:15 -0400

    changed fonts

--- script.js
+++ script.js
@@ -5,37 +5,37 @@ function newStyle() {
     switch (x){
         case 0:
             newColor = '#020100';
-            newFont = 'Arial'; 
+            newFont = 'Alegreya'; 
             break;
         case 1: 
             newColor = '#3B28CC';
-            newFont = 'Verdana, cursive'; 
+            newFont = 'Alegraya Sans'; 
             break;
         case 2:
-            newColor = '#38023B';
-            newFont = 'Tahoma';
+            newColor = '#8E0606';
+            newFont = 'Black Ops One';
             break; 
         case 3:
             newColor= '#1E6478';
-            newFont = 'Georgia';
+            newFont = 'Josefin Slab';
             break
         case 4:
             newColor = '#4B6A12';
-            newFont = 'Brush Script MT, cursive'; 
+            newFont = 'Junge'; 
             break;
         case 5:
             newColor = '#451E71';
-            newFont = 'Palatino';
+            newFont = 'Lato';
             break;
         case 6: 
             newColor = '#A1005E';
-            newFont = 'Impact';
+            newFont = 'Neucha';
             break;
         case 7:
             newColor = "#941B0C";
             newFont = "BioRhyme";
     }
-    let elem = document.getElementById('header');
+    let elem = document.getElementById('topOfScreen');
     elem.style.color = newColor;
     elem.style.fontFamily = newFont; 
   }
